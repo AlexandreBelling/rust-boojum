a02c93433ec83a8cb778183541c0adffe5f860de
alexandre.belling <alexandre.belling@consensys.net>
2019-07-07 17:37:16 +0200

    reorganization: uses fork from bellman and pairing

--- gadgets/src/fields/mnt4758/mod.rs
+++
@@ -1,5 +0,0 @@
-use bellman_ce::pairing::ff::{ PrimeField, Field }
-extern crate eth_pairing;
-
-use eth_pairings::field::{ U768 }
-
--- gadgets/src/fields/mod.rs
+++ gadgets/src/fields/mod.rs
@@ -1,3 +1,4 @@
 extern crate bellman_ce;
 mod fp2;
+
 pub use fp2::{ Fp2Parameters, Fp2 };
